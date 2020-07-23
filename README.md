e162a83410d06a2735ea158a9a850f6e60c1525c
Ahsan <ahsan13043@gmail.com>
2020-07-23 23:43:50 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deployment-django-on-pythonanywhere
---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, re_path, path
+
+from . import views
+
+#TEMPLATE TAGGING
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path(r'^relative/$', views.relative, name='relative'),
+    re_path(r'^other/$',views.other,name='other')
+
+
+]
